cd271701138f200bbe16bd45ad67f485060cfced
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2018-02-24 15:53:01 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { Login } from './login';
 import { Component } from '@angular/core';
+import {FormGroup, FormBuilder,Validators, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  login: Login = new Login;
+
+  userDetailsForm:FormGroup;
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+
+  constructor(private formBuilder:FormBuilder){
+  
+    this.userDetailsForm = this.formBuilder.group({
+      'email': ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      'password': ['',  Validators.compose([Validators.required, Validators.minLength(5)])]
+     });
+    console.log(this.login)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
